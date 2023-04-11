3e0ea0edc6804cc3580b670114e3c9f2c91ed05b
Matheus Silverio <matheusphillipo@gmail.com>
2023-04-10 22:35:55 -0300

    Create table transactions

--- db/migrations/20230322012534_create-transactions.ts
+++ db/migrations/20230322012534_create-transactions.ts
@@ -2,7 +2,7 @@ import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable("transactions", (table) => {
-    table.uuid().primary();
+    table.uuid("id").primary();
     table.text("title").notNullable();
   });
 }
